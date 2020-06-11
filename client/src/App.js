333dfad805e5a73d20922aba20496442e0fde49c
KonstantinModin <konstantin.v.modin@gmail.com>
2020-06-11 16:03:47 +0200

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Join from "./components/Join";
+import Chat from "./components/Chat";
 
 const App = () => {
     return (
-        <div>
+        <BrowserRouter>
             <h1>App</h1>
-        </div>
+            <Route path="/" exact component={Join} />
+            <Route path="/chat" exact component={Chat} />
+        </BrowserRouter>
     );
 };
 
