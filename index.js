e9587c87effc630392c948bb9975f15d4b0cb173
KonstantinModin <konstantin.v.modin@gmail.com>
2020-06-12 19:08:07 +0200

    updates

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const io = socketio(server);
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+app.use(router);
+
 server.listen(PORT, () => console.log(`Server is running on ${PORT}`));
