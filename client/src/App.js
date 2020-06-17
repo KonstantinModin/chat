618a7a7cf2d0e0db0e75f213bfe17341ec8d81fe
KonstantinModin <konstantin.v.modin@gmail.com>
2020-06-17 05:59:57 +0200

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Join from "./components/Join";
 import Chat from "./components/Chat";
+import Header from "./Header";
 import "./App.css";
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Header />
             <Route path="/" exact component={Join} />
             <Route path="/chat" exact component={Chat} />
         </BrowserRouter>
---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h2>Real-time Chat with React / socket.io</h2>
+        </div>
+    );
+};
+
+export default Header;
