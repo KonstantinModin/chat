313e7daab616626a0c2fa3cd44da9172974ab50b
Konstantin Modin <45493372+KonstantinModin@users.noreply.github.com>
2020-06-18 15:43:10 +0200

    Update header

--- client/src/Header.js
+++ client/src/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     return (
         <div className="Header">
             <h1>Real-time Chat with React / socket.io</h1>
+            <h5>Try and use instant messages</h5>
         </div>
     );
 };
