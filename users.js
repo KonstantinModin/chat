1e02834289faea1949079ec979a03fc8921f367e
KonstantinModin <konstantin.v.modin@gmail.com>
2020-06-26 21:07:01 +0200

    updates

--- users.js
+++ users.js
@@ -1,6 +1,18 @@
 const users = [];
 
-const addUser = (user) => [...users, user];
+const removeWhiteSpaceAndLowerCase = (s) => s.replace(/ /g, "").toLowerCase();
+
+const addUser = ({ id, name, room }) => {
+    name = removeWhiteSpaceAndLowerCase(name);
+    room = removeWhiteSpaceAndLowerCase(room);
+
+    const existingUser = users.find(
+        (user) => user.room === room && user.name === name
+    );
+
+    if (existingUser) return { error: "Username is taken" };
+    users.push({ id, name, room });
+};
 
 const removeUser = (id) => users.filter((user) => user.id !== id);
 
